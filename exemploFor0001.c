00617184456f0e5d0af4c557ae3a3f42b39a3783
wagnerelvio <wagnerelvio@gmail.com>
2023-09-03 15:12:34 -0300

    Create exemploFor0001.c
    // Imprimir -de -10 -a -30 ~ comando for

---
+++ exemploFor0001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Imprimir -de -10 -a -30 ~ comando for
+
+int main ()
+{
+    for ( int i = 10; i <= 30; i ++)
+    {
+        printf (" -%d \n", i ) ;
+    }
+    return 0;
+
+}
