7109a57a4ab8007ad7c6f6621a54f04e23cd2185
wagnerelvio <wagnerelvio@gmail.com>
2023-09-03 15:19:48 -0300

    Create ExemploFor0002.c
     Imprimir os numeros de 30 a -10 ~ comando for

---
+++ ExemploFor0002.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// Imprimir os numeros de 30 a -10 ~ comando for
+
+int main ()
+{
+    for ( int i = 30; i >= 10; i--)
+    {
+        printf ("- %d \n", i ) ;
+    }
+    return 0;
+}
