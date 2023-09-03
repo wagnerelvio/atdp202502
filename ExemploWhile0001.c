a92fc62f0533af86da9d6926da878e062c8223b9
wagnerelvio <wagnerelvio@gmail.com>
2023-09-03 15:16:03 -0300

    Create ExemploWhile0001.c
     Imprimir -de -10 -a -30 ~ comando while

---
+++ ExemploWhile0001.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+// Imprimir -de -10 -a -30 ~ comando while
+
+int main ()
+{
+    int i = 10;
+    while ( i <= 30)
+    {
+        printf (" - %d \n", i) ;
+        i ++;
+    }
+    return 0;
+}
