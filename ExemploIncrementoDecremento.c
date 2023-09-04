23d159253479dfbe47b844c3a4826a19884c51bc
wagnerelvio <wagnerelvio@gmail.com>
2023-09-04 13:30:19 -0300

    Create ExemploIncrementoDecremento.c
    Operadores de Incremento e Decremento.

---
+++ ExemploIncrementoDecremento.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Operadores de Incremento e Decremento
+int main() {
+    int num = 5;
+
+    // Operador de incremento (++): aumenta o valor da variável em 1
+    num++;
+    printf("Valor após o incremento: %d\n", numero);
+
+    // Operador de decremento (--): diminui o valor da variável em 1
+    num--;
+    printf("Valor após o decremento: %d\n", numero);
+
+    return 0;
+}
