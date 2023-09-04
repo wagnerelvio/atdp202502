bb614747bcb0d6961e603f93e5f3421028feb235
wagnerelvio <wagnerelvio@gmail.com>
2023-09-04 13:32:14 -0300

    Create ExemploIdade.c
    Uso do if, else if e else  - idade.

---
+++ ExemploIdade.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//Uso do if, else if e else  - idade
+int main() {
+    int idade;
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+
+    if (idade < 18) {
+        printf("Você é menor de idade.\n");
+    } else if (idade >= 18 && idade < 65) {
+        printf("Você é um adulto.\n");
+    } else {
+        printf("Você é um idoso.\n");
+    }
+
+    return 0;
+}
