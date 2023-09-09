159daa25b9b01c017b81bb2b2d5e25f028d72dad
wagnerelvio <wagnerelvio@gmail.com>
2023-09-09 16:20:22 -0300

    Create ExemploPonteiro151.c
    Exemplo de uma variavel e ponteiro

---
+++ ExemploPonteiro151.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Exemplo de uma variavel e ponteiro
+int x = 5;
+
+int main()
+{
+
+    printf(" O valor da variavel x eh igual a %d \n\n", x);
+    printf(" O endereco hexadecimal da variavel x eh igual a %p ", &x);
+    return 0;
+}
