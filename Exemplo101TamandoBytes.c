5f4a901e421ed7ebf867746fa3d99d766274b118
wagnerelvio <wagnerelvio@gmail.com>
2023-09-09 14:12:23 -0300

    Create Exemplo101TamandoBytes.c
    Tamanho de um float em bytes.

---
+++ Exemplo101TamandoBytes.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    printf("O tamanho de um inteiro (int) em bytes é: %zu\n", sizeof(int));
+    printf("O tamanho de um caractere (char) em bytes é: %zu\n", sizeof(char));
+    printf("O tamanho de um float em bytes é: %zu\n", sizeof(float));
+    printf("O tamanho de um float em bytes é: %zu\n", sizeof(double));
+    printf("O tamanho de um float em bytes é: %zu\n", sizeof(long double));    
+    
+    return 0;
+}
