85c36a90995943775b9d955a8d0d674b82934d40
wagnerelvio <wagnerelvio@gmail.com>
2023-10-16 15:13:42 -0300

    Create funcaoemC201.c

---
+++ funcaoemC201.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Programa -- funcao -201 - 10/10/2023
+
+ int main ()
+ {
+ printf ("\n\n");
+ printf ("%c%c%c%c%c%c%c%c\n", '#','#','#','#','#','#','#','#');
+ printf ("%c%c%c%c%c%c%c%c\n", '#',' ','#','#','#','#',' ','#');
+ printf ("%c%c%c%c%c%c%c%c\n", '#',' ','#','#','#','#',' ','#');
+ printf ("%c%c%c%c%c%c%c%c\n", '#','#','#','#','#','#','#','#');
+ printf ("\n");
+ }
