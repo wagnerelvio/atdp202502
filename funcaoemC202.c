b7023030ccd23edaf67d60ac582cdffb583825b7
wagnerelvio <wagnerelvio@gmail.com>
2023-10-16 15:38:04 -0300

    Create funcaoemC202.c

---
+++ funcaoemC202.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Q '#'
+#define V ' '
+
+// Programa -- funcao - 202 - 10/10/2023
+
+ int main ()
+ {
+
+printf ("\n\n");
+ printf ("%c%c%c%c%c%c%c%c\n", Q ,Q ,Q ,Q ,Q ,Q ,Q , Q ) ;
+ printf ("%c%c%c%c%c%c%c%c\n", Q ,V ,Q ,Q ,Q ,Q ,V , Q ) ;
+ printf ("%c%c%c%c%c%c%c%c\n", Q ,V ,Q ,Q ,Q ,Q ,V , Q ) ;
+ printf ("%c%c%c%c%c%c%c%c\n", Q ,Q ,Q ,Q ,Q ,Q ,Q , Q ) ;
+ printf ("\n") ;
+ return 0;
+ }
