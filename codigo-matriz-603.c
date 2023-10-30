be11269cb114f831ca9573beaa65ccf343e3e50a
wagnerelvio <wagnerelvio@gmail.com>
2023-10-30 15:00:17 -0300

    Create codigo-matriz-603.c

---
+++ codigo-matriz-603.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int linhas, colunas;
+
+    printf("Digite o número de linhas da matriz: ");
+    scanf("%d", &linhas);
+
+    printf("Digite o número de colunas da matriz: ");
+    scanf("%d", &colunas);
+
+    int matriz[linhas][colunas];
+
+    printf("Digite os elementos da matriz:\n");
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("Digite o valor para a posição [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("A matriz inserida é:\n");
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
