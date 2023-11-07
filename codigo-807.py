ebfe57662e7b10636bc9668f96240f2ab96cdc7c
wagnerelvio <wagnerelvio@gmail.com>
2023-11-07 20:19:19 -0300

    Create codigo-807.py

---
+++ codigo-807.py
@@ -0,0 +1,10 @@
+# Solicitar ao usuário que insira três números
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+# Encontrar o maior número usando a função max()
+maior = max(num1, num2, num3)
+
+# Imprimir o resultado
+print(f"O maior número é: {maior}")
