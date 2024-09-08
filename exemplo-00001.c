43243fdd71646f41a43185047258c91ac66c5f23
wagnerelvio <wagnerelvio@gmail.com>
2024-09-07 21:49:05 -0300

    Create exemplo-00001.c

---
+++ exemplo-00001.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Inclui a biblioteca padrao de entrada e saida
+
+int main() {
+    printf("Hello, World!\n"); // Exibe a mensagem "Hello, World!" na tela
+    return 0; // Indica que o programa terminou com sucesso
+}
