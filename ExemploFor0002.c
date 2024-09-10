01ad2e7ff90b0423d9289ed0f177cf00fb9440f1
wagnerelvio <wagnerelvio@gmail.com>
2024-09-10 20:55:46 -0300

    Update ExemploFor0002.c

--- ExemploFor0002.c
+++ ExemploFor0002.c
@@ -8,6 +8,9 @@ int main ()
     for ( int i = 30; i >= 10; i--)
     {
         printf ("- %d \n", i ) ;
+        printf (" A vida eh bela! \n") ;
+        
     }
+    printf (" Hoje eh Terca-feira! \n") ;
     return 0;
 }
