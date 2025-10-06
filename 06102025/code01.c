2fc763086a0e6225eb1e6425a398752a42fd829c
wagnerelvio <wagnerelvio@gmail.com>
2025-10-06 19:28:39 -0300

    Update code01.c

--- 06102025/code01.c
+++ 06102025/code01.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 int main() {
-    printf("Números pares entre 0 e 20:\n");
+    printf("Números pares entre 0 e 20 (usando o resto):\n");
 
-    // O loop 'for' começa em 0, vai até 20 (inclusive) e incrementa de 2 em 2
-    for (int i = 0; i <= 20; i = i + 2) {
-        printf("%d\n", i);
+    // O loop 'for' itera por TODOS os números de 0 a 20 (incrementando de 1 em 1)
+    for (int i = 0; i <= 20; i++) {
+        
+        // Verificação do resto:
+        // O operador '%' (módulo) retorna o resto da divisão de 'i' por 2.
+        // Se o resto for 0, o número é PAR.
+        if (i % 2 == 0) {
+            printf("%d\n", i);
+        }
     }
 
     return 0;
