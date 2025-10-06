4dcac5d9c1a1a868f11e9308a3ce5aef3b3aa5dc
wagnerelvio <wagnerelvio@gmail.com>
2025-10-06 19:30:19 -0300

    Create code02.c
    .

---
+++ 06102025/code02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("Números pares entre 0 e 20 (usando o resto):\n");
+
+    // O loop 'for' itera por TODOS os números de 0 a 20 (incrementando de 1 em 1)
+    for (int i = 0; i <= 20; i++) {
+        
+        // Verificação do resto:
+        // O operador '%' (módulo) retorna o resto da divisão de 'i' por 2.
+        // Se o resto for 1, o número é IMPAR.
+        if (i % 2 == 1) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
