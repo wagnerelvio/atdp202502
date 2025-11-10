dc900419df35d7b3ca18c57bcdeb2cf4d44144b9
wagnerelvio <wagnerelvio@gmail.com>
2025-11-10 09:12:30 -0300

    Create Code-01.c

---
+++ Aula-EscopoPdP02/Code-01.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+//
+void dobrar (int n) {
+n = n * 2;
+}
+ int main () {
+ int numero = 5;
+ dobrar ( numero );
+ printf (" Numero = %d\n", numero ); // Saida : 5
+return 0;
+ }
